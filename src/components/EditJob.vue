e97d693d8a8cc5b79a82a30667e6b12a3412c222
byrdsd <seanbyrd@yahoo.com>
2021-01-24 19:47:01 -0600

    Fix create

--- src/components/EditJob.vue
+++ src/components/EditJob.vue
@@ -2,16 +2,16 @@
   <form class="EditJob" @submit.prevent>
     <div class="EditJob-control">
       <label class="EditJob-label" for="title">Job Title</label>
-      <input v-model="title" type="text" class="EditJob-input" name="title">
+      <input v-model="jobData.title" type="text" class="EditJob-input" name="title">
     </div>
     <div class="EditJob-control">
       <label class="EditJob-label" for="descripiton">Job Description</label>
-      <input v-model="description" type="text" class="EditJob-input" name="description">
+      <input v-model="jobData.description" type="text" class="EditJob-input" name="description">
     </div>
     <div class="EditJob-control">
       <label class="EditJob-label" for="skills">Skills</label>
-      <input v-for="(skill, index) in job.skills" v-bind:key="index" v-model="skills[index]" type="text" class="EditJob-input" name="skills">
-      <input v-model.lazy="skills[skills.length]" type="text" class="EditJobs-input" name="skills">
+      <input v-for="(skill, index) in jobData.skills" v-bind:key="index" v-model="jobData.skills[index]" type="text" class="EditJob-input" name="skills">
+      <input v-model.lazy="jobData.skills[jobData.skills.length]" type="text" class="EditJobs-input" name="skills">
     </div>
     <input type="submit" class="EditJob-submit" value="Save" @click="submitForm()">
     <button class="EditJob-close" @click="closeForm()" type="button">Cancel</button>
@@ -35,9 +35,12 @@ export default {
   },
   data: function () {
     return {
-      title: this.job.title,
-      description: this.job.description,
-      skills: this.job.skills
+      jobData: {
+        id: this.job.id,
+        title: this.job.title,
+        description: this.job.description,
+        skills: this.job.skills
+      }
     }
   },
   methods: {
@@ -46,7 +49,7 @@ export default {
       this.$emit('cancel')
     },
     submitForm: function () {
-      this.$emit('job-saved', this.job);
+      this.$emit('job-saved', this.jobData);
     }
   }
 }
